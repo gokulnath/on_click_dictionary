f7d4c1fe268069e17e8a66458fe705b3a2a29aeb
Gokulnath Manakkattil <gokulnathonline@gmail.com>
2013-07-29 23:14:40 +0530

    Fix wordnick fetcher.

--- lib/dictionary/fetch.rb
+++ lib/dictionary/fetch.rb
@@ -5,13 +5,24 @@ class Fetch
 
   def initialize
     Wordnik.configure do |config|
-     	config.api_key = Common.settings[:wordnick_api_key]
+      config.api_key = Common.settings[:wordnick_api_key]
     end
   end
 
   def meaning(word)
-    definition = Wordnik.word.get_definitions(word).first # wordnik returans an array of all defnitions
-    definition['text']
+    begin
+      definition = Wordnik.word.get_definitions(word).first # wordnik returans an array of all defnitions
+    rescue ClientError => e
+      puts e.message
+    rescue ServerError => e
+      puts e.message
+    end
+
+    begin
+      return definition['text'] if definition
+    rescue TypeError => e
+      puts e.message  
+    end 
   end
 
 end
