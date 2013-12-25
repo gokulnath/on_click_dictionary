b17bda280cb7b780fe98a3d2c8aa4814d595c05c
Gokulnath Manakkattil <gokulnathonline@gmail.com>
2013-12-25 17:25:31 +0530

    Refactor commit.
    Intendation of common.rb was messy, fixed that. Also the same file
    has a duplicate in lib folder, removed it too.

--- lib/dictionary/common.rb
+++ lib/dictionary/common.rb
@@ -1,24 +1,24 @@
 class Common
 
-	class << self
+  class << self
 
-		def settings
-			read_config 'config.yml'
-		end
+    def settings
+      read_config 'config.yml'
+    end
 
-		private
+    private
 
     def settings_dir
       File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
     end
 
     def read_config(filename)
-      settings =  File.open(File.join(settings_dir, filename)) do |f|
+      settings = File.open(File.join(settings_dir, filename)) do |f|
         YAML.load f
       end
       settings.with_indifferent_access
     end
 
-	end
+  end
 
 end
